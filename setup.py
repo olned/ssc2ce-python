e41ccca0689067fed8f33e7236317d3fbb7a347c
Oleg Nedbaylo <olned64@gmail.com>
2019-04-24 12:57:47 +0200

    setup was updated. Added long_description and classifiers

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@
 from distutils.core import setup
 from deribit.VERSION import __version__
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='ons-deribit',
       version=__version__,
       description='Simple Deribit API v2 on Websocket',
@@ -10,5 +13,10 @@ setup(name='ons-deribit',
       author_email='olned64@gmail.com',
       url='https://github.com/olned/ons-deribit-ws-python',
       packages=['deribit'],
-      install_requires=['aiohttp', ]
+      install_requires=['aiohttp', ],
+      classifiers=[
+              "Programming Language :: Python :: 3.6",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+          ],
       )
