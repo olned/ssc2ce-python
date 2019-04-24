15f094279fee0e1055696b8de620a4c73067ad1a
Oleg Nedbaylo <olned64@gmail.com>
2019-04-24 16:10:15 +0200

    resolve_route supports str and Pattern as key

--- deribit/utils.py
+++ deribit/utils.py
@@ -1,11 +1,29 @@
 import re
+from typing import Pattern
 
 
 def resolve_route(value, routes):
     key, handler = None, None
     for key, handler in routes:
-        if key and re.match(key, value):
-            return handler
+        if key:
+            if isinstance(key, str):
+                if key == value:
+                    return handler
+            elif isinstance(key, Pattern) and re.match(key, value):
+                return handler
 
     if key is not None and key == "" and handler:
         return handler
+
+
+def hide_secret(request):
+    data = request.copy()
+    hidden_keys = ["password", "username", "client_id", "client_secret", "refresh_token", "access_token"]
+
+    for key, value in data.items():
+        if isinstance(value, dict):
+            data[key] = hide_secret(value)
+        elif key in hidden_keys:
+            data[key] = "***"
+
+    return data
