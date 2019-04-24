b23f16ef7abea630e6bf0de93fe777f158761fab
Oleg Nedbaylo <olned64@gmail.com>
2019-04-24 15:25:24 +0200

    empty_handler added

--- examples/public.py
+++ examples/public.py
@@ -2,62 +2,55 @@
 
 import asyncio
 import logging
-from pprint import pprint
 
 from deribit import Deribit
 from deribit.VERSION import __version__
 
-logging.basicConfig(format='%(asctime)s %(name)s %(funcName)s %(levelname)s %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(name)s %(funcName)s %(levelname)s %(message)s', level=logging.WARNING)
 logger = logging.getLogger("ons-derobit-ws-python-sample")
 
-app = Deribit()
+conn = Deribit()
 
 
-async def start_public():
-    await app.send_public(request={
+async def hello():
+    await conn.send_public(request={
         "method": "public/hello",
         "params": {
-            "client_name": "TCC Deribit Connector",
+            "client_name": "ONS Deribit",
             "client_version": __version__
         }
     })
-    await app.set_heartbeat(13)
-    await app.send_public(request={
+
+    await conn.send_public(request={
         "method": "public/subscribe",
         "params": {
-            "channels": ["book.BTC-PERPETUAL.raw"]
+            "channels": ["deribit_price_index.btc_usd"]
         }
     })
 
 
 async def printer(data):
-    pprint(data)
+    method = data.get("method")
+    if method and method == "subscription":
+        if data["params"]["channel"].startswith("deribit_price_index"):
+            index_name = data["params"]["data"]["index_name"]
+            price = data["params"]["data"]["price"]
+            print(f" Deribit Price Index {index_name.upper()}: {price}")
 
+conn.on_connect_ws = hello
+conn.on_message = conn.handle_message
 
-app.on_connect_ws = start_public
-app.on_message = app.handle_message
-
-app.method_routes += [
+conn.method_routes += [
     ("subscription", printer),
 ]
 
 loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
 
-def disable_heartbeat():
-    asyncio.ensure_future(app.disable_heartbeat())
-
-
-def stop():
-    asyncio.ensure_future(app.stop())
-
-
-loop.call_later(60, stop)
-
 try:
-    loop.run_until_complete(app.run_receiver())
+    loop.run_until_complete(conn.run_receiver())
     logger.info("Application was stopped.")
 except KeyboardInterrupt:
     logger.info("Application closed by KeyboardInterrupt.")
 
-app.close()
+conn.close()
