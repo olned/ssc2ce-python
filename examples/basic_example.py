9c08bced05a1ddec444d0968ebcbf8dfa941c5ca
Oleg Nedbaylo <olned64@gmail.com>
2019-04-24 15:34:25 +0200

    Replaced public.py to basic_example.py

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python
-
 import asyncio
-import logging
-
 from deribit import Deribit
-from deribit.VERSION import __version__
-
-logging.basicConfig(format='%(asctime)s %(name)s %(funcName)s %(levelname)s %(message)s', level=logging.WARNING)
-logger = logging.getLogger("ons-derobit-ws-python-sample")
 
 conn = Deribit()
 
 
-async def hello():
-    await conn.send_public(request={
-        "method": "public/hello",
-        "params": {
-            "client_name": "ONS Deribit",
-            "client_version": __version__
-        }
-    })
-
+async def subscribe():
     await conn.send_public(request={
         "method": "public/subscribe",
         "params": {
@@ -29,7 +14,7 @@ async def hello():
     })
 
 
-async def printer(data):
+async def handle_subscription(data):
     method = data.get("method")
     if method and method == "subscription":
         if data["params"]["channel"].startswith("deribit_price_index"):
@@ -37,20 +22,13 @@ async def printer(data):
             price = data["params"]["data"]["price"]
             print(f" Deribit Price Index {index_name.upper()}: {price}")
 
-conn.on_connect_ws = hello
-conn.on_message = conn.handle_message
 
-conn.method_routes += [
-    ("subscription", printer),
-]
+conn.on_connect_ws = subscribe
+conn.method_routes += [("subscription", handle_subscription)]
 
 loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
-
 try:
     loop.run_until_complete(conn.run_receiver())
-    logger.info("Application was stopped.")
 except KeyboardInterrupt:
-    logger.info("Application closed by KeyboardInterrupt.")
-
-conn.close()
+    print("Application closed by KeyboardInterrupt.")
