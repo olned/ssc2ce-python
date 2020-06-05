a92928751e58be007682418d2506703a2abf1820
Oleg Nedbaylo <olned64@gmail.com>
2020-06-05 12:02:52 +0200

    get_currencies and get_instruments were added callback added to the send methods

--- ssc2ce/deribit.py
+++ ssc2ce/deribit.py
@@ -74,10 +74,15 @@ class Deribit(SessionWrapper):
         ]
 
     async def run_receiver(self):
+        """
+        Establish a connection and start the receiver loop.
+        :return:
+        """
         self.ws = await self._session.ws_connect(self.ws_api)
         if self.on_connect_ws:
             await self.on_connect_ws()
 
+        # A receiver loop
         while self.ws and not self.ws.closed:
             message = await self.ws.receive()
             self.receipt_time = time()
@@ -96,19 +101,49 @@ class Deribit(SessionWrapper):
             if self.on_message:
                 await self.on_message(message)
 
-    async def send_public(self, request):
+    def close(self):
+        super()._close()
+
+    async def stop(self):
+        """
+        Close connection and break the receiver loop
+        :return:
+        """
+        await self.ws.close()
+
+    async def send_public(self, request: dict, callback=None) -> int:
+        """
+        Send a public request
+
+        :param request: Request without jsonrpc and id fields
+        :param callback: The function that will be called after receiving the query result. Default is None
+        :return: Request Id
+        """
         request_id = self.get_id()
         request = {
             "jsonrpc": "2.0",
             "id": request_id,
             **request
         }
-        self.requests[request_id] = request
         self.logger.info(f"sending:{repr(hide_secret(request))}")
         await self.ws.send_json(request)
+
+        if callback:
+            request["callback"] = callback
+
+        self.requests[request_id] = request
+
         return request_id
 
-    async def send_private(self, request):
+    async def send_private(self, request: dict, callback=None) -> int:
+        """
+        Send a private request
+
+        :param request: Request without jsonrpc and id fields
+        :param callback: The function that will be called after receiving the query result. Default is None
+        :return: Request Id
+        """
+
         request_id = self.get_id()
         access_token = self.auth_params["access_token"]
         request["params"]["access_token"] = access_token
@@ -118,21 +153,38 @@ class Deribit(SessionWrapper):
             "id": request_id,
             **request
         }
-        self.requests[request_id] = request
         self.logger.info(f"sending:{repr(hide_secret(request))}")
         await self.ws.send_json(request)
+
+        if callback:
+            request["callback"] = callback
+
+        self.requests[request_id] = request
+
         return request_id
 
-    async def send(self, request):
+    async def send(self, request: dict, callback=None) -> int:
+        """
+        A wrapper for send_private and send_public, defines the type of request by content.
+
+        :param request: Request without jsonrpc and id fields
+        :param callback: The function that will be called after receiving the query result. Default is None
+        :return: Request Id
+        """
         method = request["method"]
         if method.starts("public/"):
-            request_id = await self.send_public(request)
+            request_id = await self.send_public(request, callback)
         else:
-            request_id = await self.send_private(request)
+            request_id = await self.send_private(request, callback)
 
         return request_id
 
-    async def auth_login(self):
+    async def auth_login(self) -> int:
+        """
+        Send an authentication request using parameters stored in the constructor
+
+        :return: Reuest id
+        """
         self.auth_type = AuthType.CREDENTIALS
 
         msg = {
@@ -150,8 +202,9 @@ class Deribit(SessionWrapper):
         request_id = await self.send_public(msg)
         return request_id
 
-    async def auth_client_credentials(self, client_id, client_secret, scope: str = None):
+    async def auth_client_credentials(self, client_id, client_secret, scope: str = None) -> int:
         """
+        Send a credentials authentication request
 
         :param client_id: using the access key
         :param client_secret: and access secret that can be found on the API page on the website
@@ -160,7 +213,7 @@ class Deribit(SessionWrapper):
             trade:[read, read_write, none],
             wallet:[read, read_write, none],
             account:[read, read_write, none]
-        :return:
+        :return: Request Id
         """
 
         self.auth_type = AuthType.CREDENTIALS
@@ -183,7 +236,20 @@ class Deribit(SessionWrapper):
         request_id = await self.send_public(msg)
         return request_id
 
-    async def auth_password(self, username, password, scope: str = None):
+    async def auth_password(self, username: str, password: str, scope: str = None) -> int:
+        """
+        Send a password authentication request
+
+        :param username: User name
+        :param password: Password
+        :param scope:
+            connection, session, session:name,
+            trade:[read, read_write, none],
+            wallet:[read, read_write, none],
+            account:[read, read_write, none]
+        :return: Request Id
+        """
+
         self.auth_type = AuthType.PASSWORD
         msg = {
             "method": "public/auth",
@@ -203,7 +269,12 @@ class Deribit(SessionWrapper):
         request_id = await self.send_public(msg)
         return request_id
 
-    async def auth_refresh_token(self):
+    async def auth_refresh_token(self) -> int:
+        """
+        Send a refresh token request
+
+        :return: Request Id
+        """
         msg = {
             "method": "public/auth",
             "params": {
@@ -215,7 +286,12 @@ class Deribit(SessionWrapper):
         request_id = await self.send_public(msg)
         return request_id
 
-    async def auth_logout(self):
+    async def auth_logout(self) -> int:
+        """
+        Send a logout request
+
+        :return: Request Id
+        """
         msg = {
             "method": "private/logout",
             "params": {}
@@ -224,15 +300,54 @@ class Deribit(SessionWrapper):
         request_id = await self.send_private(msg)
         return request_id
 
-    async def set_heartbeat(self, interval: int = 15):
+    async def set_heartbeat(self, interval: int = 15) -> int:
+        """
+
+        :param interval:
+        :return:
+        """
         request_id = await self.send_public({"method": "public/set_heartbeat", "params": {"interval": interval}})
         return request_id
 
-    async def disable_heartbeat(self):
+    async def disable_heartbeat(self) -> int:
+        """
+
+        :return:
+        """
         request_id = await self.send_public({"method": "public/disable_heartbeat", "params": {}})
         return request_id
 
-    async def handle_message(self, message: aiohttp.WSMessage):
+    async def get_currencies(self, callback=None):
+        """
+        Send a request for a list of cryptocurrencies supported by the API
+        :param callback:
+        :return: Request Id
+        """
+        return await self.send_public(request=dict(method="public/get_currencies", params={}), callback=callback)
+
+    async def get_instruments(self, currency: str, kind: str = None, callback=None) -> int:
+        """
+        Send a request for a list available trading instruments
+        :param currency: The currency symbol: BTC or ETH
+        :param kind: Instrument kind: future or option, if not provided instruments of all kinds are considered
+        :param callback:
+        :return: Request Id
+        """
+        request = {"method": "public/get_instruments",
+                   "params": {
+                       "currency": currency
+                   }}
+        if kind:
+            request["kind"] = kind
+
+        return await self.send_public(request=request, callback=callback)
+
+    async def handle_message(self, message: aiohttp.WSMessage) -> None:
+        """
+
+        :param message:
+        :return:
+        """
         if message.type == aiohttp.WSMsgType.TEXT:
             data = message.json()
             self.logger.debug(f"handling:{repr(hide_secret(data))}")
@@ -250,7 +365,11 @@ class Deribit(SessionWrapper):
                         request_id = data["id"]
                         request = self.requests.get(request_id)
                         if request:
-                            await self.handle_response(request=request, response=data)
+                            if "callback" in request:
+                                callback = request["callback"]
+                                await callback(data)
+                            else:
+                                await self.handle_response(request=request, response=data)
 
                             del self.requests[request_id]
                         else:
@@ -266,10 +385,21 @@ class Deribit(SessionWrapper):
         else:
             self.logger.warning(f"Unknown type of message {repr(message)}")
 
-    async def empty_handler(self, **kwargs):
+    async def empty_handler(self, **kwargs) -> None:
+        """
+        A default handler
+        :param kwargs:
+        :return:
+        """
         self.logger.debug(f"{repr(kwargs)}")
 
-    async def handle_response(self, request, response):
+    async def handle_response(self, request, response) -> None:
+        """
+
+        :param request:
+        :param response:
+        :return:
+        """
         method = request["method"]
         handler = resolve_route(method, self.response_routes)
 
@@ -277,9 +407,13 @@ class Deribit(SessionWrapper):
             return await handler(request=request, response=response)
 
         self.logger.warning(f"Unhandled method:{method} response:{repr(response)} to request:{repr(request)}.")
-        return
 
-    async def handle_method_message(self, data):
+    async def handle_method_message(self, data) -> None:
+        """
+
+        :param data:
+        :return:
+        """
         method = data["method"]
         handler = resolve_route(method, self.method_routes)
 
@@ -287,15 +421,23 @@ class Deribit(SessionWrapper):
             return await handler(data)
 
         self.logger.warning(f"Unhandled message:{repr(data)}.")
-        return
 
     async def handle_heartbeat(self, data):
+        """
+
+        :param data:
+        :return:
+        """
         if data["params"]["type"] == "test_request":
             await self.send_public({"method": "public/test", "params": {}})
 
-        return
+    async def handle_auth(self, request, response) -> None:
+        """
 
-    async def handle_auth(self, request, response):
+        :param request:
+        :param response:
+        :return:
+        """
         self.auth_params = response["result"]
         grant_type = request["params"]["grant_type"]
         if grant_type == "":
@@ -311,9 +453,3 @@ class Deribit(SessionWrapper):
             pass
         else:
             self.logger.error(f"Unknown grant_type {repr(hide_secret(request))} : {repr(hide_secret(response))}")
-
-    def close(self):
-        super()._close()
-
-    async def stop(self):
-        await self.ws.close()
