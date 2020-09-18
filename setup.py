1d27934e7cd8e77a676b59e0871ada4b68fe3e83
Oleg Nedbaylo <olned64@gmail.com>
2020-09-18 18:10:00 +0000

    0.14.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ssc2ce',
-    version="0.14.1",
+    version="0.14.2",
     author='Oleg Nedbaylo',
     author_email='olned64@gmail.com',
     description='A Set of Simple Connectors for access To Cryptocurrency Exchanges',
