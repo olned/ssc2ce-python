879bdf0fd26e17e03970294ae6109d1215699850
Oleg Nedbaylo <olned64@gmail.com>
2020-09-18 18:06:20 +0000

    example updated

--- examples/deribit/check_is_connected.py
+++ examples/deribit/check_is_connected.py
@@ -4,21 +4,34 @@ from ssc2ce import Deribit
 
 conn = Deribit()
 
-print("Is connected after init", conn.is_connected())
-
-
-def close():
-    print("Check connection  before closing", conn.is_connected())
-    asyncio.ensure_future(conn.stop())
-
-
 loop = asyncio.get_event_loop()
 
+i = 0
+def check():
+    global i
+    i += 1
+    print("Check connection  ", conn.is_connected())
+    loop.call_later(1, close, i < 2)
 
-loop.call_later(1, close)
+def run():
+    print("Is connected before run_receiver() ", conn.is_connected())
+    asyncio.ensure_future(conn.run_receiver())
+    loop.call_later(1, check)
+
+def close(restart: bool = True):
+    print("Is connection before closing", conn.is_connected())
+    asyncio.ensure_future(conn.stop())
+    if restart:
+        loop.call_later(2, run)
+
+def stop():
+    loop.call_later(1, loop.stop)
+
+loop.call_later(10, stop)
+loop.call_soon(run)
 
 try:
-    loop.run_until_complete(conn.run_receiver())
+    loop.run_forever()
 except KeyboardInterrupt:
     print("Application closed by KeyboardInterrupt.")
 
